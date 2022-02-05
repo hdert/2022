c09f5d64fb6f6597c61c47baf307f8c7673d42b4
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2022-02-06 12:39:08 +1300

    increase buffer size

--- rust/image_combiner/src/main.rs
+++ rust/image_combiner/src/main.rs
@@ -20,7 +20,7 @@ struct FloatingImage {
 
 impl FloatingImage {
     fn new(width: u32, height: u32, name: String) -> Self {
-        let buffer_capacity = 3_655_744;
+        let buffer_capacity: usize = 25_000_000;
         let buffer: Vec<u8> = Vec::with_capacity(buffer_capacity);
         FloatingImage {
             width,
